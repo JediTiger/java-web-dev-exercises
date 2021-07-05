9df27941a882776248aa06f53f04d4a3e43625b0
JediTiger <JediTiger@users.noreply.github.com>
2021-07-04 21:42:07 -0500

    Updates

---
+++ src/OneB/ArrayPractice.java
@@ -0,0 +1,12 @@
+package OneB;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        for (int arrayStuff : integerArray) {
+            if () { // number is odd, print. If not, don't
+                System.out.println(arrayStuff);
+            }
+        }
+    }
+}
