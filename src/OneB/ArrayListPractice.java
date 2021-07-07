ea4c46d89b8f9a7cc62c3578d69f74e3ac7f67f2
JediTiger <JediTiger@users.noreply.github.com>
2021-07-07 18:47:53 -0500

    Updates

--- src/OneB/ArrayListPractice.java
+++ src/OneB/ArrayListPractice.java
@@ -3,19 +3,32 @@ package OneB;
 import java.util.ArrayList;
 
 public class ArrayListPractice {
-
+// Part 1b
     static Integer sumEvenValues(ArrayList<Integer> intValues) {
         int sumTotal = 0;
         int counter = 0;
         for ( int i = 0; i < intValues.size()+1; i++ ) {
             if (i % 2 == 0) {
-                counter = i++;
+                counter = counter + 1;
                 sumTotal = sumTotal + i;
             }
         }
+        System.out.println("Number of integers counted: " + counter);
         return sumTotal;
     }
 
+    static void printSpecificWords(Integer searchText) {
+        String phrase = "Little fuzzy was a bear. Little fuzzy was a pear. Little fuzzy was a mare. Little fuzzy had no care.";
+        String[] phraseArr = phrase.split(" ");
+        System.out.println(phraseArr);
+        for ( String item : phraseArr ) {
+            if (item.length() == searchText) {
+                System.out.println(item);
+            }
+        }
+    }
+
+// Part 1a
     public static void main(String[] args) {
         ArrayList<Integer> values = new ArrayList<>();
         for (int i = 1; i < 21; i++) {
@@ -24,6 +37,8 @@ public class ArrayListPractice {
         System.out.println("The values used for computation are: " + values);
 
         System.out.println("The total of the even integers is: " + sumEvenValues(values));
+
+        printSpecificWords(5);
    }
 
 }
