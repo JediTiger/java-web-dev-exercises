295266aef72f49a9cd6256b9401b07f98e3d5e6d
JediTiger <JediTiger@users.noreply.github.com>
2021-07-07 21:03:57 -0500

    Initial build of Studio1B.java

--- src/studios/OneB/Studio1B.java
+++ src/studios/OneB/Studio1B.java
@@ -1,4 +1,19 @@
 package studios.OneB;
 
+import java.util.HashMap;
+
 public class Studio1B {
+    public static void main(String[] args) {
+        String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        String[] phraseAsChars = phrase.toCharArray().toString();
+        // For Map, key will be the character, value will be the count
+        // For making the Map, the initial counter value will be 0 for all entries
+        HashMap<String,Integer> x = new HashMap<>();
+        // Converting the character array into the Map collection
+        // This loop will create keys from the characters and set the initial value to zero for all keys
+        for (String item : phraseAsChars) {
+            x.put(item,0);
+        }
+        System.out.println(x);
+    }
 }
