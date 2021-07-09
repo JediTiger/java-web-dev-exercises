239377979045e88180a47ddea6a7462f09ecd23e
JediTiger <JediTiger@users.noreply.github.com>
2021-07-09 12:25:02 -0500

    Working on lesson 5 Unit Testing

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -2,11 +2,9 @@ package org.launchcode.java.demos.lsn3classes1;
 
 import java.util.*;
 
-protected class Course {
-    private class Course {
-        private String className;
-        private Integer level;
-        private Teacher teacher;
-        private ArrayList<Student> enrolledStudents;
-    }
+public class Course {
+    private String className;
+    private Integer level;
+    private Teacher teacher;
+    private ArrayList<Student> enrolledStudents;
 }
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -5,6 +5,21 @@ import java.util.*;
 public class SchoolPractice {
     private static Object Student;
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
     private static void main(String[] args) {
 
         Student students = new Student("Sean", 1, 1, 4.0);
