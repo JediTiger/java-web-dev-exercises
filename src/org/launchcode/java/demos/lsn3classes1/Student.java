d1c3c0caafcbb219292e91826b291c47fe3c548b
JediTiger <JediTiger@users.noreply.github.com>
2021-07-08 22:35:01 -0500

    Initial build on Student in

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,31 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0 );
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    protected int getID() {
+        return studentId;
+    }
+
+    private void setID(int aStudentId) {
+        studentId = aStudentId;
+    }
 }
--- src/studios/OneB/Studio1B.java
+++ src/studios/OneB/Studio1B.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Studio1B {
     public static void main(String[] args) {
         String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        String[] phraseAsChars = phrase.toCharArray().toString();
+        String[] phraseAsChars = phrase.split("");
         // For Map, key will be the character, value will be the count
         // For making the Map, the initial counter value will be 0 for all entries
         HashMap<String,Integer> x = new HashMap<>();
@@ -15,5 +15,9 @@ public class Studio1B {
             x.put(item,0);
         }
         System.out.println(x);
+        // 2nd loop will iterate thru the Map and increment the values of each
+//        for (HashMapitem : x) {
+            //
+ //       }
     }
 }
