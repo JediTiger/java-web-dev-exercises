88da26437f5f2ea53d55e7e48f640f831c66b635
JediTiger <JediTiger@users.noreply.github.com>
2021-07-11 20:43:13 -0500

    Working on Exercises 8 (Week 4a)

--- src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -1,4 +1,27 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
-public class ConeComparator {
+import java.util.Comparator;
+public class ConeComparator implements Comparator<Cone> {
+    @Override
+    public int compare(Cone o1, Cone o2) {
+
+//        if/else if/else block to evaluate o1.getCost() - o2.getCost();
+
+        System.out.println("Cone A cost");
+        System.out.println(o1.getCost());
+        System.out.println("Cone B cost");
+        System.out.println(o2.getCost());
+        System.out.println("Cones ");
+        System.out.println(o1.getCost() - o2.getCost());
+
+        if (o1.getCost() > o2.getCost() ) {
+            return 1;
+        }
+        else if (o1.getCost() < o2.getCost()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -9,23 +9,31 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
-        System.out.println("\nFlavors");
+        System.out.println("\nFlavors before sort");
         System.out.println(flavors);
-        System.out.println("\nCones");
-        System.out.println(cones);
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
 
-        Comparator comparator = new FlavorComparator();
+        Comparator comparatorF = new FlavorComparator();
 
-        flavors.sort(comparator);
+        flavors.sort(comparatorF);
 
         System.out.println("\nFlavors after sort:");
         System.out.println(flavors);
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+
+        System.out.println("\nCones before sort");
+        System.out.println(cones);
+
+        Comparator comparatorC = new ConeComparator();
+
+        cones.sort(comparatorC);
+
+        System.out.println("\nCones after sort:");
+        System.out.println(cones);
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
